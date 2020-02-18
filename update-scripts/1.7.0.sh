4664039e65beeb0838d7652693dfd2521448469b
Kyle Moad <kyle.moad@gmail.com>
2020-02-18 16:00:35 -0500

    change to 1.7.0 update script

--- update-scripts/1.7.0.sh
+++ update-scripts/1.7.0.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 sudo pip3 install open-cravat -U
-sudo sed -i 's/\r$//g' /usr/local/lib/python3.6/site-packages/cravat/cravat_web.py
-sudo patch /usr/local/lib/python3.6/site-packages/cravat/cravat_web.py <<EOF
-46,49d45
-<     log_handler = logging.handlers.TimedRotatingFileHandler(log_path, when='d', backupCount=30)
-<     log_formatter = logging.Formatter('%(asctime)s: %(message)s', '%Y/%m/%d %H:%M:%S')
-<     log_handler.setFormatter(log_formatter)
-<     logger.addHandler(log_handler)
-166a163,166
->     log_handler = logging.handlers.TimedRotatingFileHandler(log_path, when='d', backupCount=30)
->     log_formatter = logging.Formatter('%(asctime)s: %(message)s', '%Y/%m/%d %H:%M:%S')
->     log_handler.setFormatter(log_formatter)
->     logger.addHandler(log_handler)
-EOF
+logDir=/usr/local/lib/python3.6/site-packages/cravat/logs
+sudo mkdir -p $logDir
+log=$logDir/wcravat.log
+sudo touch $log
+sudo chmod a+rw $log
 sudo env "PATH=$PATH" oc config md /mnt/ssd/oc
 sudo chown centos:centos /mnt/ssd/oc/* -R
 oc module update -y
